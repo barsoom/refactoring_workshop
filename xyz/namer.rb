bd87337e809c24e708a5f14107b4ec51db743bcc
Henrik Nyh <henrik@nyh.se>
2015-04-17 12:29:46 +0200

    Cleanup ;)

--- xyz/namer.rb
+++ xyz/namer.rb
@@ -4,21 +4,32 @@ module XYZ
   module Namer
 
     def self.xyz_filename(target)
-      # File format:
-      # [day of month zero-padded][three-letter prefix] \
-      # _[kind]_[age_if_kind_personal]_[target.id] \
-      # _[8 random chars]_[10 first chars of title].jpg
-      filename = "#{target.publish_on.strftime("%d")}"
-      filename << "#{target.xyz_category_prefix}"
-      filename << "#{target.kind.gsub("_", "")}"
+      filename = ""
+
+      # Day of month zero-padded.
+      filename << target.publish_on.strftime("%d")
+
+      # Three-letter prefix.
+      filename << target.xyz_category_prefix
+
+      # Kind.
+      filename << target.kind.gsub("_", "")
+
+      # Zero-padded age if personal.
       filename << "_%03d" % (target.age || 0) if target.personal?
-      filename << "_#{target.id.to_s}"
-      filename << "_#{Digest::SHA1.hexdigest(rand(10000).to_s)[0,8]}"
-      truncated_title = target.title.gsub(/[^\[a-z\]]/i, '').downcase
-      truncate_to = truncated_title.length > 9 ? 9 : truncated_title.length
-      filename << "_#{truncated_title[0..(truncate_to)]}"
+
+      filename << "_#{target.id}"
+
+      # Random chars.
+      filename << "_#{Digest::SHA1.hexdigest(rand(10000).to_s)[0, 8]}"
+
+      # Title prefix.
+      truncated_title = target.title.downcase.gsub(/[^\[a-z\]]/, "")
+      filename << "_#{truncated_title[0..9]}"
+
       filename << ".jpg"
-      return filename
+
+      filename
     end
 
   end
